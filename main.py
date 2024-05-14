4c1b9a29294d86adacd72c86e0455bbdbec36d83
Igna Valdivia <ignacio.valdivia@alumnos.frm.utn.edu.ar>
2024-05-14 12:35:33 -0300

    api

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-from flask import Flask, render_template, request
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 from deepface import DeepFace
 import os
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/analyze':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    if 'image' not in request.files:
-        return "No file part"
+            # Guardar la imagen en un archivo temporal
+            temp_image_path = 'static/uploads/temp_image.jpg'
+            with open(temp_image_path, 'wb') as temp_image_file:
+                temp_image_file.write(post_data)
 
-    file = request.files['image']
+            # Llamar a la función analyze_data()
+            result = analyze_data(temp_image_path)
+            print(result)
 
-    if file.filename == '':
-        return "No selected file"
+            # Eliminar el archivo después de usarlo
+            os.remove(temp_image_path)
 
-    # Guardar la imagen cargada en la carpeta 'uploads'
-    file.save('static/uploads/' + file.filename)
+            # Enviar la respuesta
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(result).encode())
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
+
+
+
+def analyze_data(path):
 
     try:
         # Analizar edad, género, raza y emociones utilizando DeepFace
-        result = DeepFace.analyze(img_path='static/uploads/' + file.filename,
+        result = DeepFace.analyze(img_path= path,
                                   actions=('age', 'gender', 'race', 'emotion'))
 
         # Obtener la edad estimada
@@ -38,16 +54,25 @@ def analyze():
         # Obtener la emoción dominante
         emotion = result[0]['emotion']
 
-        # Eliminar el archivo después de usarlo
-        os.remove('static/uploads/' + file.filename)
-
         return f"Age: {age}, Gender: {gender}, Race: {race}, Emotion: {emotion}"
+
+
 
     except ValueError as e:
         # Eliminar el archivo después de usarlo
-        os.remove('static/uploads/' + file.filename)
         return "No se ha podido detectar un rostro en la imagen. Por favor, carga una imagen con un rostro visible."
 
 
+def run_server(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print('Server stopped.')
+
 if __name__ == '__main__':
-    app.run()
+    run_server()
